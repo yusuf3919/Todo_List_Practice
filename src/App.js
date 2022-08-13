e5a2ad726f1e08fbea09005e6c867c22921909b9
Mohammed Yusuf Abdul Lateef <89645307+yusuf3919@users.noreply.github.com>
2022-08-13 23:33:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import IconLabelButtons from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <IconLabelButtons />
+    </div>
+  );
+}
+export default App;
